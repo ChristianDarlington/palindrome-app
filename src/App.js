aa809d6e641233b6ac73eb09ce72ca0654d07a55
Christian Darlington <bocacode@bocacodes-MacBook-Air.local>
2022-11-03 17:16:38 -0400

    words reverse in console

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  let palendrome = []
+  let input;
+    
+  const handleInput = (e) => {
+     input = e.target.value
+    console.log(input) 
+  }
+
+  const reverseWord = (value) => {
+    for (let i = value.length - 1; i > -1; i--) {
+      const elem = value[i];
+      console.log(elem)
+      palendrome.push()
+    }
+  }
+
+  const checkIfPalindrome = (value) => {
+    reverseWord(value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='main'>
+      <div className='input-area'>
+        <input type='text' onChange={(e) => handleInput(e)}/>
+       <button onClick={() => checkIfPalindrome(input)}>Submit </button>
+      </div>
+         <div>yaaay!!</div>
+      </div>
     </div>
   );
 }
